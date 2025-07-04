c4ea79524bae5399cbcfa8bb39392faa0766c236
aakashpho <aakash.phoughat@gmail.com>
2025-07-04 21:56:31 +0530

    add gemini model for summarizer

--- summarize/src/main.py
+++ summarize/src/main.py
@@ -1,16 +1,21 @@
 import os
 import logging
-from transformers import pipeline
+import argparse
+from dotenv import load_dotenv
+import google.generativeai as genai
 
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
-def run_summarizer():
-    summarizer = pipeline("summarization", model="t5-small")
+def get_path_of_file(file_name):
     base_dir = os.path.dirname(__file__)
-    input_path = os.path.join(base_dir,"..","data","input.txt")
+    path = os.path.join(base_dir,"..","data",file_name)
+    return path
+
+def get_input_text():
+    input_path = get_path_of_file("input.txt")
     try:
         with open(input_path, "r",encoding="utf-8") as file:
             article = file.read()
@@ -23,12 +28,51 @@ def run_summarizer():
     if not article.strip():
         logging.error("Input file is empty")
         exit(1)
-    summary = summarizer(article, max_length=130, min_length=30, do_sample=True)
-    if summary and 'summary_text' in summary[0]:
-        logging.info(summary[0]['summary_text'])
+    return article
+
+def write_output_file(response_text):
+    output_path = get_path_of_file("output.txt")
+    try:
+        with open(output_path, "w",encoding="utf-8") as file:
+            file.write(response_text)
+        logging.info(f"Summary written to {output_path}")
+    except FileNotFoundError:
+        logging.error(f"File not found: {output_path}")
+        exit(1)
+    except Exception as e:
+        logging.error(f"Error writing to file: {e}")
+
+def run_summarizer(gemini_api_key,model_name):
+    genai.configure(api_key = gemini_api_key)
+
+    system_prompt = "You are a highly capable summarization engine. Your task is to extract the most important information and main ideas from any provided text and present them as a concise, accurate, and easy-to-understand summary. Do not add outside information or interpretations. Focus solely on condensing the given content while preserving its core meaning and context."
+    model = genai.GenerativeModel(model_name,system_instruction = system_prompt) 
+    article = get_input_text()
+    response = model.generate_content(article)
+    if response.text :
+        write_output_file(response.text)
     else:
         logging.error("No summary generated")
         exit(1)
 
+def get_keys(model):
+    key_name = get_key_name(model)
+    return os.getenv(key_name)
+
+def get_key_name(model):
+    if "gemini" in model:
+        return "GEMINI_API_KEY"
+
+def parse_argument():
+    parser = argparse.ArgumentParser(description="Summarize a text file using Gemini API")
+    parser.add_argument("--model",type=str,default="gemini-2.5-flash",help="Model_name")
+    return parser.parse_args()
+
 if __name__ == "__main__":
-    run_summarizer()
+    load_dotenv()
+    args = parse_argument()
+    gemini_api_key = get_keys(args.model)
+    if not gemini_api_key:
+        logging.error("Gemini API key not found. Please set it in your .env file")
+        exit(1)
+    run_summarizer(gemini_api_key,args.model)
